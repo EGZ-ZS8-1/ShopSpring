d2baf1fc0257b25e5a1c9f5aae6c41af3ea570f6
EGZ-ZS8-1 <maiklinfo@mail.ru>
2024-01-02 16:29:48 +0200

    Добавил пакет controller и класс в нём DefaultController

--- src/main/java/com/example/ShopSpring/controller/DefaultController.java
+++ src/main/java/com/example/ShopSpring/controller/DefaultController.java
@@ -1,9 +1,25 @@
 package com.example.ShopSpring.controller;
 
 
+import com.example.ShopSpring.entity.ProductType;
+import com.example.ShopSpring.repository.ProductTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
+
+
+    @Autowired
+    ProductTypeRepository productTypeRepository;
+
+    @GetMapping("/")
+    public String index (Model model) {
+        Iterable<ProductType> types =productTypeRepository.findAll();
+        model.addAttribute("types",types);
+        return "index";
+    }
 
 }
